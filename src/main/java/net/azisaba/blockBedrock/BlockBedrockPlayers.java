21c80d4bfe551f06005404e0bb2f7dee0a72c78c
acrylic-style <me@acrylicstyle.xyz>
2021-10-31 18:24:20 +0900

    Add config.yml to customize the kick message

--- src/main/java/net/azisaba/blockBedrock/BlockBedrockPlayers.java
+++ src/main/java/net/azisaba/blockBedrock/BlockBedrockPlayers.java
@@ -8,17 +8,21 @@ import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockBedrockPlayers extends JavaPlugin {
+    private String kickMessage = "";
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new TheGreatEventListener(), this);
+        getConfig().options().copyDefaults();
+        kickMessage = getConfig().getString("kickMessage", "&cBedrock Edition (統合版)では参加できません。");
         getLogger().info("Enabled " + this.getName());
     }
 
-    public static class TheGreatEventListener implements Listener {
+    public class TheGreatEventListener implements Listener {
         @EventHandler
         public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent e) {
             if (e.getName().startsWith(".")) {
-                e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, ChatColor.RED + "Bedrock Edition (統合版)では参加できません。");
+                e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, ChatColor.translateAlternateColorCodes('&', kickMessage));
             }
         }
     }
